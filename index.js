2639eabcb5aad31030100e8470ef44b997743ac0
Ryan Wang <wongyouth@gmail.com>
2023-03-08 14:00:42 +0800

    Add comments

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
 (function ($) {
-  var originDatepicker = $.fn.datepicker;
+  var datepicker = $.fn.datepicker;
 
   $.fn.datepicker = function (options) {
     this.each(function () {
       var el = $(this);
 
+      // get date format for submission
       var uniformFormat =
         options.uniformFormat || el.data("date-uniform-format") || "yyyy-mm-dd";
 
-      // add alt field for datepicker input
+      // add hidden input to store normalized date string for submission
       var dateEl = $(`<input type="hidden">`).attr("name", el.attr("name"));
       el.attr("name", null);
 
-      originDatepicker.call(el, options).on("changeDate", (e) => {
+      // store date when changed
+      datepicker.call(el, options).on("changeDate", (e) => {
         dateEl.val(e.format(uniformFormat));
       });
 
-      originDatepicker.call(el, "setDate", el.val());
+      // store initial date
+      datepicker.call(el, "setDate", el.val());
 
+      // add input to DOM tree
       el.after(dateEl);
     });
   };
